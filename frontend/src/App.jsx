88c92ed07af586761407b97c1beaa5146c8fbc5e
martynas <martynaskvietelaitis@icloud.com>
2026-01-21 11:58:15 +0200

    init for frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/layout/header'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-eneba flex flex-col">
+      <Header />
+      <main>
+      </main>
+    </div>
+  )
+}
+
+export default App
